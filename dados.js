e7af31d056964d44311657a12ac7a64f2e719598
Gustavo Thiago Ribeiro Farina <161526981+GustavoHransa@users.noreply.github.com>
2024-09-06 18:21:42 -0400

    Projeto Alura

---
+++ dados.js
@@ -0,0 +1,26 @@
+let dados = [
+    {//Vectra
+
+        titulo: "Chevrolet Vectra",
+        descricao:"Baseado no Opel Vectra europeu, lançado originalmente na Alemanha, em 1988, o Chevrolet Vectra foi produzido pela General Motors do Brasil entre os anos de 1993 e 2011.",
+        link : "https://pt.wikipedia.org/wiki/Chevrolet_Vectra",
+        tags: "Chevrolet vectra 2.4 16v familia 2, vectra cd , vectra gls , vectra elite"
+    
+    },
+    {//Omega
+
+        titulo: "Chevrolet Omega",
+        descricao:"O Chevrolet Omega é um automóvel que foi fabricado pela General Motors no Brasil (com a marca Chevrolet), na Europa (com a marca Opel) e na Austrália (com a marca Holden)",
+        link : "https://pt.wikipedia.org/wiki/Chevrolet_Omega",
+        tags : "chevrolet omega cd fitipaldi , lotus omega , omega gls"
+    
+    },
+    {//Blazer
+
+        titulo: "Chevrolet Blazer",
+        descricao:"No Brasil, começou a ser fabricada a 2ª geração da Blazer como 1ª em 1995 para substituir a Chevrolet Veraneio. Foi fabricada com motor de 4 cilindros 2.2 EFI, de 106 cv, onde havia no sistema apenas 1 bico injetor, que anos mais tarde seria substituído pelo sistema MPFI com 4 bicos injetores, que gerariam 113 cv. No final de 1995, dois novos motores, um 4 cilindros 2.5 turbodiesel de 95 cv e o 4.3 V6 Sfi, de 180 cv. Em 1998, a primeira versão 4x4, com o motor 2.5, começa a ser vendida.",
+        link : "https://pt.wikipedia.org/wiki/Chevrolet_Blazer",
+        tags: "Blazer executive v6 4.3 SUV suv colina dlx maxion"
+    
+    }
+];
